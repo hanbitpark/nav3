acad9fd47871839889b3b551516f301fc3bb1062
hanbitpark <romaniankai@gmail.com>
2016-12-16 01:49:11 +0900

    start

--- app/src/main/java/com/example/admin/navi3/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/admin/navi3/MyFirebaseMessagingService.java
@@ -9,9 +9,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.PowerManager;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -25,84 +28,59 @@ import java.net.URLConnection;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "FirebaseMessingService";
-//    Context mContext = getApplicationContext();
-
-
-    @Override
-    public void sendBroadcast(Intent intent) {
-        super.sendBroadcast(intent);
-        Log.d(TAG, "sendBroadcast실행");
-    }
-
-    @Override
-    public void onMessageSent(String s) {
-        super.onMessageSent(s);
-    }
-
-    public MyFirebaseMessagingService() {
-        super();
-    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-
         Log.d(TAG, "onCreate실행");
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-//        try {
-//            JSONObject obj = new JSONObject(messageBody);
-//            imgUrl = obj.getString("imgUrl");
-//        }catch(Exception e){
-//
-//        }
-//
-//        if(imgUrl != "") {
-//            try {
-//                URL url = new URL(imgUrl);
-//                URLConnection conn = url.openConnection();
-//                conn.connect();
-//
-//                BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
-//                Bitmap img = BitmapFactory.decodeStream(bis);
-//
-//                NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle(notificationBuilder);
-//                style.bigPicture(img).setBigContentTitle(title);
-//            } catch (Exception e) {
-//
-//            }
-//        }
+        Log.d(TAG, "push message : " + remoteMessage.getData().toString());
+        Log.d(TAG, "push message : " + remoteMessage.getData());
 
 
         String title = remoteMessage.getData().get("title");
         String message = remoteMessage.getData().get("message");
 
-        Log.d("title", title);
-        Log.d("mesage", message);
+//        Log.d("title", title);
+//        Log.d("mesage", message);
 
+        Context mContext = getApplicationContext();
+        RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.activity_intro);
+
+
+//        remoteViews.setTextViewText();
         System.out.println("received message : " + message);
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.choi22);
 
-        Notification.Builder notificationBuilder = new Notification.Builder(this)
-                .setSmallIcon(R.mipmap.choi11)
-//                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.choi22) )
-                .setTicker("미리보기")
-                .setContentTitle(title)
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-//                .setWhen(System.currentTimeMillis())
-//                .setPriority(Notification.PRIORITY_MAX)
-                .setContentIntent(pendingIntent);
+        Notification.Builder notificationBuilder = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            notificationBuilder = new Notification.Builder(this)
+                    .setSmallIcon(R.mipmap.choi11)
+                    //setvisibility 잠금화면에서 보여주기 public 전체콘텐츠보여주기, secret 어떤부분도 안보여줌 private, 기본정보만 보여줌
+                    .setVisibility(android.support.v7.app.NotificationCompat.VISIBILITY_PUBLIC)
+    //                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.choi22) )
+                    .setTicker("미리보기")
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setAutoCancel(true)
+                    .setVibrate(new long[] {500,100,500,100})
+                    .setSound(defaultSoundUri)
+//                    .setCustomContentView(remoteViews)
+    //                .setWhen(System.currentTimeMillis())
+    //                .setPriority(Notification.PRIORITY_MAX)
+                    .setContentIntent(pendingIntent);
+        }
 
         notificationBuilder.setStyle(new Notification.BigPictureStyle()
                 .bigPicture(bitmap)
@@ -116,6 +94,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         wakelock.acquire(5000);
 
         notificationManager.notify(999 /* ID of notification */, notificationBuilder.build());
+
+
     }
 
 }
